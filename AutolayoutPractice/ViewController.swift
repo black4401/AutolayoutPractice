54751f18db659e0dd6e191201f40dcde43484a53
Alexander Angelov <alexander.angelov@mentormate.com>
2023-03-29 12:21:37 +0300

    Initial Commit

---
+++ AutolayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutolayoutPractice
+//
+//  Created by Alexander Angelov on 29.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
