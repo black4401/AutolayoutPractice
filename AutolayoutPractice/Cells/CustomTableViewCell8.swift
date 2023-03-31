2657db7cb9e7b9f73ac9592e1a64bd4d21305d56
Alexander Angelov <alexander.angelov@mentormate.com>
2023-03-31 12:54:12 +0300

    Add edit icon for the pain rate cell and small tweaks

--- AutolayoutPractice/Cells/CustomTableViewCell8.swift
+++ AutolayoutPractice/Cells/CustomTableViewCell8.swift
@@ -16,12 +16,18 @@ class CustomTableViewCell8: UITableViewCell {
     @IBOutlet weak var painLabel: UILabel!
     @IBOutlet weak var severitylabe: UILabel!
     @IBOutlet weak var painLocationLabel: UILabel!
-    
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var circleView: UIView!
+    @IBOutlet weak var editIcon: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        backView.layer.cornerRadius = 12
         iconView.image = UIImage(named: "sun_icon")
         emojiView.image = UIImage(named: "emoji_progress_icon")
+        circleView.layer.cornerRadius = 25
+        editIcon.image = UIImage(named: "edit_icon 1")
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
