2e19abaad4566d2f92c565f7093a8ecb7083cb82
Alexander Angelov <alexander.angelov@mentormate.com>
2023-04-03 18:42:16 +0300

    Tweaks for PainRateCell

--- AutolayoutPractice/Cells/PainRateCell.swift
+++ AutolayoutPractice/Cells/PainRateCell.swift
@@ -20,6 +20,7 @@ class PainRateCell: UITableViewCell {
     @IBOutlet weak var circleView: UIView!
     @IBOutlet weak var editIcon: UIImageView!
     @IBOutlet weak var innerImageView: UIImageView!
+    @IBOutlet weak var lineView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -29,6 +30,7 @@ class PainRateCell: UITableViewCell {
         innerImageView.image = UIImage(named: "anguish")
         circleView.layer.cornerRadius = 25
         editIcon.image = UIImage(named: "edit_icon 1")
+        lineView.image = UIImage(named: "Line 13")
         
         painLocationLabel.layer.cornerRadius = 6
         painLocationLabel.clipsToBounds = true
