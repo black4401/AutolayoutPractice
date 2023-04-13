da4e2f7d3e74b0d7c46823d59ff877374026489b
Alexander Angelov <alexander.angelov@mentormate.com>
2023-04-13 12:38:33 +0300

    Add the new custom colors to the extension

--- AutolayoutPractice/UIColor+Extension.swift
+++ AutolayoutPractice/UIColor+Extension.swift
@@ -12,8 +12,11 @@ extension UIColor {
     static let greyscale120 = UIColor(named: "greysca;e120")
     static let greyscale100 = UIColor(named: "greyscale100")
     static let greyscale80 = UIColor(named: "greyscale80")
+    static let greyscale10 = UIColor(named: "greyscale10")
+    static let greyscale05 = UIColor(named: "greyscale05")
     static let brandMainColor = UIColor(named: "brand_color")
     static let brandAccentTitn = UIColor(named: "brandAccentTint")
     static let yellow10 = UIColor(named: "yellow10")
     static let redCritical100 = UIColor(named: "redCritical100")
+    static let brandWhite = UIColor(named: "brandWhite")
 }
