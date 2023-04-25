db2ee6ce1780bb43f77ff61ba3463841dca6dc7b
Alexander Angelov <alexander.angelov@mentormate.com>
2023-04-25 14:54:02 +0300

    Add HorizontalScrollView

--- AutolayoutPractice/Cells/PainLocationCell.swift
+++ AutolayoutPractice/Cells/PainLocationCell.swift
@@ -12,31 +12,19 @@ class PainLocationCell: UITableViewCell {
     var collectionViewModels: [TagModel] = [] {
         didSet {
             continueButton.titleLabel?.text = "Continue (\(collectionViewModels.count))"
+            horizontalScrollView.data = collectionViewModels
         }
     }
     
+    @IBOutlet weak var horizontalScrollView: HorizontalScrollView!
     @IBOutlet weak var mainLabel: UILabel!
-    @IBOutlet weak var cellCollectionView: UICollectionView!
     @IBOutlet weak var continueButton: UIButton!
-    
-    func setUpCollectionView() {
-        cellCollectionView.register(UINib(nibName: "TagCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: CellIdentifiers.tagCellIdentifier)
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.minimumInteritemSpacing = 4
-        
-        cellCollectionView.collectionViewLayout =  layout
-    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        cellCollectionView.dataSource = self
-        cellCollectionView.delegate = self
         setUpMainLabel()
         setUpContinueButton()
-        setUpCollectionView()
     }
     
     func setUpMainLabel() {
@@ -54,43 +42,3 @@ class PainLocationCell: UITableViewCell {
     }
 }
 
-extension PainLocationCell: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, TagCollectionViewCellDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return collectionViewModels.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = cellCollectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifiers.tagCellIdentifier, for: indexPath) as! TagCollectionViewCell
-        let model = collectionViewModels[indexPath.row]
-        cell.configureCell(with: collectionViewModels[indexPath.row])
-        cell.delegate = model.hasCloseButton ? self : nil
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let label = UILabel()
-        let model = collectionViewModels[indexPath.row]
-        let spaceForButton = model.hasCloseButton ? 10 : 0
-        label.text = collectionViewModels[indexPath.row].labelText
-        let size = label.sizeThatFits(CGSize(width: 200, height: 100))
-        return CGSize(width: size.width + CGFloat(spaceForButton) + model.horizontalPadding*2, height: size.height + model.verticalPadding*2)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return collectionViewModels[indexPath.row].isSelectable
-    }
-    
-    func didTapClose(on cell: TagCollectionViewCell) {
-        
-        if let indexPath = self.cellCollectionView.indexPath(for: cell) {
-            self.cellCollectionView.performBatchUpdates {
-                collectionViewModels.remove(at: indexPath.item)
-                self.cellCollectionView.deleteItems(at: [indexPath])
-            }
-        }
-    }
-}
---
+++ AutolayoutPractice/Views/HorizontalScrollView.swift
@@ -0,0 +1,95 @@
+//
+//  HorizontalScrollableView.swift
+//  AutolayoutPractice
+//
+//  Created by Alexander Angelov on 25.04.23.
+//
+
+import UIKit
+
+class HorizontalScrollView: UIView {
+    
+    var data: [TagModel] = []
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        loadNib()
+    }
+    
+    override func awakeFromNib() {
+        setUpCollectionView()
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.register(UINib(nibName: "TagCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: CellIdentifiers.tagCellIdentifier)
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = 4
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.collectionViewLayout =  layout
+    }
+    
+    private func loadNib() {
+        let nib = UINib(nibName: "HorizontalScrollView", bundle: nil)
+        guard let contentView = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+            fatalError("Failed to load HorizontalScrollView XIB")
+        }
+        
+        addSubview(contentView)
+    }
+}
+
+extension HorizontalScrollView: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifiers.tagCellIdentifier, for: indexPath) as! TagCollectionViewCell
+        let model = data[indexPath.row]
+        cell.configureCell(with: data[indexPath.row])
+        cell.delegate = model.hasCloseButton ? self : nil
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let label = UILabel()
+        let model = data[indexPath.row]
+        let spaceForButton = model.hasCloseButton ? 10 : 0
+        label.text = data[indexPath.row].labelText
+        let size = label.sizeThatFits(CGSize(width: 200, height: 100))
+        return CGSize(width: size.width + CGFloat(spaceForButton) + model.horizontalPadding*2, height: size.height + model.verticalPadding*2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return data[indexPath.row].isSelectable
+    }
+}
+
+extension HorizontalScrollView: TagCollectionViewCellDelegate {
+    func didTapClose(on cell: TagCollectionViewCell) {
+        if let indexPath = self.collectionView.indexPath(for: cell) {
+            self.collectionView.performBatchUpdates {
+                data.remove(at: indexPath.item)
+                self.collectionView.deleteItems(at: [indexPath])
+            }
+        }
+    }
+}
